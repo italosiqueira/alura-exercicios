fe46a1808f7c9a36a82a1dd54d13d6c4a088551d
Italo Siqueira Lima <italosiqueira@gmail.com>
2018-03-16 10:00:54 -0300

    Adding helper class DateHelper; using fast fail on constructor; using static function modifier.

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,18 @@
+class DateHelper {
+
+    constructor() {
+        throw new Error("Not a instatiable class!");
+    }
+
+    static textoParaData(dateString) {
+        if(/^\d{4}-\d{2}-\d{2}$/.test(dateString)) {
+            return new Date(...dateString.split('-').map((item, index) => item - (index % 2)));
+        } else {
+            throw new Error("Not a valid Date String format!");
+        }
+    }
+    
+    static dataParaTexto(dateObject) {
+        return `${dateObject.getFullYear()}-${dateObject.getMonth() + 1}-${dateObject.getDate()}`;
+    }
+}
