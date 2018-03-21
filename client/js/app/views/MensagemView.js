6dc6565aec826b530765dea0d563b6b3cdf5ec9b
Italo Siqueira Lima <italosiqueira@gmail.com>
2018-03-21 00:48:42 -0300

    Preventing message displaying when empty (first visit).

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -1,7 +1,7 @@
 class MensagemView extends View {
 
     template(model) {
-        return `<p class="alert alert-info">${model.texto}</p>`;
+        return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : `<p></p>` ;
     }
 
 }
