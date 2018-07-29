04f708338094e14147b5b94eb20b00d3ca3348a0
Italo Siqueira Lima <italosiqueira@gmail.com>
2018-07-29 08:17:05 -0300

    Created methods to remove entities and sub-entities using the HTTP Method DELETE

--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,11 +1,16 @@
 package br.com.alura.loja.resource;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.thoughtworks.xstream.XStream;
 
@@ -24,11 +29,20 @@ public class ProjetoResource {
 	}
 	
 	@POST
-	@Produces(MediaType.APPLICATION_XML)
-	public String adiciona(String conteudo) {
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response adiciona(String conteudo) {
 		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
-		new ProjetoDAO().adiciona(projeto);		
-		return "<status>sucesso</status>";
+		new ProjetoDAO().adiciona(projeto);
+		URI uri = URI.create("/projetos/" + projeto.getId());
+		return Response.created(uri).build();
+	}
+	
+	@Path("{id}")
+	@DELETE
+	public Response remove(@PathParam("id") long id) {
+		new ProjetoDAO().remove(id);
+		
+		return Response.ok().build();
 	}
 
 }
