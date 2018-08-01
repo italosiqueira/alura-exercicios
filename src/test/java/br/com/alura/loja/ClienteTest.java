5b71a7e020681f54433f6b9790e1fc01895b53c6
Italo Siqueira Lima <italosiqueira@gmail.com>
2018-08-01 01:21:17 -0300

    Setting client logging.

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -13,6 +13,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,7 +34,9 @@ public class ClienteTest {
 	@Before
 	public void inicializarServidor() throws URISyntaxException {
 		this.servidor = Servidor.startaServidor();
-		client = ClientBuilder.newClient();
+		ClientConfig config = new ClientConfig();
+		config.register(new LoggingFilter());
+		client = ClientBuilder.newClient(config);
 		target = client.target("http://localhost:8080");
 	}
 	
