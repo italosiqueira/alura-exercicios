a92e6ad3c228487788ae3f7d3af5eb87426d2185
Italo Siqueira Lima <italosiqueira@gmail.com>
2019-01-28 16:28:08 -0300

    Implementa filtro de monitoramento tempo de execução das requisições.

---
+++ gerenciador/src/br/com/caelum/gerenciador/acao/MonitoramentoFilter.java
@@ -0,0 +1,27 @@
+package br.com.caelum.gerenciador.acao;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/entrada")
+public class MonitoramentoFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		long before = System.currentTimeMillis();
+		
+		chain.doFilter(request, response);
+		
+		long after = System.currentTimeMillis();
+		
+		System.out.println("Request time: " + (after - before) + " ms");
+	}
+
+}
