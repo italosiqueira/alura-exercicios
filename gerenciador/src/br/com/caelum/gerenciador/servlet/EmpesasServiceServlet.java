51281940e9cb2703d5cadd1212d70ff9795572f3
Italo Siqueira Lima <italosiqueira@gmail.com>
2019-01-29 16:38:28 -0300

    Adiciona um novo servlet que serve conteúdo em JSON e XML.
    Adicionado também as bibliotecas para geração do JSON e XML.

---
+++ gerenciador/src/br/com/caelum/gerenciador/servlet/EmpesasServiceServlet.java
@@ -0,0 +1,56 @@
+package br.com.caelum.gerenciador.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+
+import br.com.caelum.gerenciador.modelo.Banco;
+import br.com.caelum.gerenciador.modelo.Empresa;
+
+/**
+ * Servlet implementation class EmpesasServiceServlet
+ */
+@WebServlet("/empresas")
+public class EmpesasServiceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Banco banco = new Banco();
+		
+		List<Empresa> empresas = banco.getEmpresas();
+		
+		String content = asXmlContent(empresas);
+		
+		response.getWriter().append(content);
+	}
+
+	private String asJsonContent(List<Empresa> empresas) {
+		Gson gson = new Gson();
+		
+		String jsonContent = gson.toJson(empresas);
+		return jsonContent;
+	}
+
+	private String asXmlContent(List<Empresa> empresas) {
+		XStream stream = new XStream();
+		
+		stream.alias("empresas", List.class);
+		stream.alias("empresa", Empresa.class);
+		
+		String xmlContent = stream.toXML(empresas);
+		
+		return xmlContent;
+	}
+}
