df640fd5240c86c59b84378ec9f7c5575205c25f
Italo Siqueira Lima <italosiqueira@gmail.com>
2019-01-29 23:46:30 -0300

    Adapta o projeto para deploy no Jetty.
    Implementa os métodos init e destroy da interface Filter.

--- gerenciador/src/br/com/caelum/gerenciador/servlet/ControladorFilter.java
+++ gerenciador/src/br/com/caelum/gerenciador/servlet/ControladorFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -16,7 +17,17 @@ import br.com.caelum.gerenciador.acao.IAcao;
 
 @WebFilter("/entrada")
 public class ControladorFilter implements Filter {
-
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+	}
+	
+	@Override
+	public void destroy() {
+		
+	}
+	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
