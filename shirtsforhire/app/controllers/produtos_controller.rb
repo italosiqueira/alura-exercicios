4068de681ee4d905deb6b89bdeb5074f00cdeefb
Italo Siqueira Lima <italosiqueira@gmail.com>
2019-07-06 07:33:22 -0300

    Adiciona o formulário de criação de produto.

--- shirtsforhire/app/controllers/produtos_controller.rb
+++ shirtsforhire/app/controllers/produtos_controller.rb
@@ -3,4 +3,7 @@ class ProdutosController < ApplicationController
         @produtos_por_nome = Produto.order(:nome)
         @produtos_por_preco = Produto.order(:preco).reverse_order.limit 2
     end
+
+    def create
+    end
 end
