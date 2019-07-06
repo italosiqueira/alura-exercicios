8c423e79312efbaf843b1f6c04a0ca7911913119
Italo Siqueira Lima <italosiqueira@gmail.com>
2019-07-06 08:29:37 -0300

    Usa a especificação resources para geração das rotas.

--- shirtsforhire/config/routes.rb
+++ shirtsforhire/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "produtos" => "produtos#index"
-  get "produtos/new" => "produtos#new"
-  post "produtos" => "produtos#create"
-  get "produtos/:id/remove" => "produtos#destroy"
+  resources :produtos, only: [:new, :create, :destroy]
   root "produtos#index"
 end
