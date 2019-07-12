d677c4b5e1d07b2a82a5f42dd95b3138d39ce428
Italo Siqueira Lima <italosiqueira@gmail.com>
2019-07-12 17:09:44 -0300

    Refatora o controller; cria lógica de atualização.

--- shirtsforhire/app/controllers/produtos_controller.rb
+++ shirtsforhire/app/controllers/produtos_controller.rb
@@ -1,4 +1,7 @@
 class ProdutosController < ApplicationController
+    
+    before_action :set_produto, only: [:edit, :destroy, :update]
+
     def index
         @produtos_por_nome = Produto.order(:nome)
         @produtos_por_preco = Produto.order(:preco).reverse_order.limit 2
@@ -6,36 +9,55 @@ class ProdutosController < ApplicationController
 
     def new
         @produto = Produto.new
-        @departamentos = Departamento.all
+        renderiza :new
     end
     
     def create
-        entrada = params.require(:produto).permit :nome, :descricao, :quantidade, :preco, :departamento_id
-        @produto = Produto.new entrada
+        @produto = Produto.new produto_params
         if @produto.save
             flash[:notice] = "Novo produto cadastrado com sucesso!"
             redirect_to root_url
         else
-            @departamentos = Departamento.all
-            render :new
+            renderiza :new
         end
     end
     
     def edit
-        id = params[:id]    #busca o parâmetro "id"
-        @produto = Produto.find(id)    #procura o produto
-        @departamentos = Departamento.all
-        render :new  #queremos renderizar a view do "new", pois é no formulário que faremos as modificações
+        renderiza :edit
+    end
+
+    def update
+        if @produto.update produto_params
+            flash[:notice] = "Produto atualizado com sucesso!"
+            redirect_to root_url
+        else
+            renderiza :edit
+        end 
     end
     
     def destroy
-        id = params[:id]
-        Produto.destroy id
+        @produto.destroy
         redirect_to root_url
     end
 
     def busca
         @nome_busca = params[:nome]
         @resultados = Produto.where "nome LIKE ?", "%#{@nome_busca}%"
+    end
+
+    private
+
+    def set_produto
+        id = params[:id]    #busca o parâmetro "id"
+        @produto = Produto.find(id)    #procura o produto
+    end
+    
+    def produto_params
+        params.require(:produto).permit :nome, :descricao, :quantidade, :preco, :departamento_id
+    end
+
+    def renderiza visao
+        @departamentos = Departamento.all
+        render visao
     end
 end
