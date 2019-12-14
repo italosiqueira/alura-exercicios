8bed08021eb23a1d466b5ed26403e18310d73248
Italo Siqueira Lima <italosiqueira@gmail.com>
2019-12-14 08:23:22 -0300

    Registra o Interceptor e muda o mecanismo de logging no Resource.

--- agendamentoemail/src/main/java/br/com/alura/resource/AgendamentoEmailResource.java
+++ agendamentoemail/src/main/java/br/com/alura/resource/AgendamentoEmailResource.java
@@ -1,11 +1,8 @@
 package br.com.alura.resource;
 
 import java.util.List;
-import java.util.logging.Logger;
 
-import javax.ejb.EJBException;
 import javax.inject.Inject;
-import javax.validation.ConstraintViolationException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -19,8 +16,6 @@ import br.com.alura.entity.AgendamentoEmail;
 
 @Path("/agendamentoemail")
 public class AgendamentoEmailResource {
-	
-	private static Logger log = Logger.getLogger(AgendamentoEmailResource.class.getName());
 	
 	@Inject
 	private AgendamentoEmailBusiness business;
@@ -37,23 +32,8 @@ public class AgendamentoEmailResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response salvarAgendamentoEmail(AgendamentoEmail agendamentoEmail ) {
-		
-		try {
-			business.salvarAgendamentoEmail(agendamentoEmail);
-		} catch (EJBException e) {
-			if (e.getCause() instanceof ConstraintViolationException) {
-				log.info(e.getMessage());
-			} else {
-				log.severe(e.getMessage());
-			}
-			
-			// para o mapeamento de exceções continuar capturando as exceções 
-			// e mostrar a mensagem na tela
-			throw e;
-		}
-		
+		business.salvarAgendamentoEmail(agendamentoEmail);
 		return Response.status(201).build();
-	
 	}
 
 }
